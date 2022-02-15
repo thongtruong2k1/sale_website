6ddb962a643d9d11983c613c2870fe788f3de095
Nguyen Quoc Long <quoclongdng@gmail.com>
2022-02-15 08:21:37 +0700

    new

---
+++ resources/views/admin/shares/menu.blade.php
@@ -0,0 +1,20 @@
+<div class="scrollbar-sidebar">
+    <div class="app-sidebar__inner">
+        <ul class="vertical-nav-menu">
+            <li class="app-sidebar__heading">Dashboards</li>
+            <li>
+                <a href="index.html" class="mm-active">
+                    <i class="metismenu-icon pe-7s-rocket"></i>
+                    Dashboard Example 1
+                </a>
+            </li>
+            <li class="app-sidebar__heading">UI Components</li>
+            <li>
+                <a href="#">
+                    <i class="metismenu-icon pe-7s-diamond"></i>
+                    Elements
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
